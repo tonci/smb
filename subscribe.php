0fc316df53ee9ccb168642799aff01078752af63
root <root@raspberrypi>
2014-01-05 22:52:23 +0000

    change the include path

--- subscribe.php
+++ subscribe.php
@@ -3,7 +3,7 @@ function db_encode($s) {
     $s = stripslashes($s);
     return mysql_real_escape_string($s);
 }
-$path = "/var/www/smb/";
+$path = "/var/www/smb";
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 // Connecting, selecting database
 // $link = mysql_connect('localhost', 'root', '123')
@@ -17,7 +17,7 @@ $email = $_POST['email'];
 // Performing SQL query
 $query = 'INSERT INTO subscribers (subscriber_email) VALUES ("'.db_encode($email).'")';
 $result = @mysql_query($query); /* or die('Query failed: ' . mysql_error());*/
-require './lib/PHPMailerAutoload.php';
+require 'lib/PHPMailer/PHPMailerAutoload.php';
 
 $mail = new PHPMailer;
 
@@ -29,7 +29,7 @@ $mail->Password = 'secret';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted*/
 
 $mail->From = 'teodor.p.nikolov@gmail.com';
-$mail->FromName = 'Tonci';
+//$mail->FromName = 'Tonci';
 $mail->addAddress($email);  // Add a recipient
 // $mail->addAddress('ellen@example.com');               // Name is optional
 // $mail->addReplyTo('info@example.com', 'Information');
@@ -41,8 +41,8 @@ $mail->addAddress($email);  // Add a recipient
 // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->Subject = 'Thanks for Subscribing!';
+$mail->Body    = 'You have beed subscribed to SMB';
 $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 if(!$mail->send()) {
@@ -54,7 +54,7 @@ if(!$mail->send()) {
 // echo 'Message has been sent';
 
 
-header("Location: /s2b/?thanks=1");
+header("Location: /smb/?thanks=1");
 // Printing results in HTML
 /*echo "<table>\n";
 while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
